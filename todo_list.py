93b12ec4b6b53756a6d78ff32cba8305c0a9ffb2
allancodeman <allancodeman@163.com>
2020-11-28 16:19:31 +0800

    change favicon and re-edit the base.html's footer.

---
+++ todo_list.py
@@ -0,0 +1,17 @@
+"""
+    :author: Allan
+    :copyright: © 2020 Yalun Hu <allancodeman@163.com>
+    :license: MIT, see LICENSE for more details.
+"""
+
+# TODO
+# 将头像图片强制命名为profile.png，读取头像的url写死，一开始创建一个纯色的profile.png作为初始的头像
+
+# 显示头像可以在html中使用此语句
+# <a href="{{ url_for('get_file', filename="截屏2020-11-16 下午11.40.22.png") }}" target="_blank">
+# <img src="{{ url_for('get_file', filename="截屏2020-11-16 下午11.40.22.png") }}">
+
+
+
+# TODO
+# 将ck-editor改为editormd,这样方便后续的公式编辑
